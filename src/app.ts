4e1fb61c98e6dcce7c1330d53753e3d41efa0f6c
Mathis Le Bonniec <mathislbonniec@gmail.com>
2020-11-15 22:41:26 +0100

    🏗 Create function to add row

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,11 @@
-import { GoogleSpreadsheetWorksheet } from 'google-spreadsheet';
 import { getBarcodes, getSkus, IBarcodes, ISkus } from './controllers/sql';
-import loadSheet from './controllers/load-sheet';
+import { addRow } from './controllers/sheet';
 
 (async () => {
-  const sheet: GoogleSpreadsheetWorksheet = await loadSheet();
-
   const barcodes: IBarcodes | null = await getBarcodes('3350033630314');
   const skus: ISkus | null = await getSkus(barcodes.skuId);
   
-  await sheet.addRow({
+  await addRow({
     'MPX': barcodes.skuId,
     'Code-barre': barcodes.barcode,
     'Asset Fill': barcodes.assetFill,
