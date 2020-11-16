60037092089df2b1a3d196d485efe29bd51dc9bf
Mathis Le Bonniec <mathislbonniec@gmail.com>
2020-11-16 18:42:08 +0100

    feat(route): add sku route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,6 @@ import sku from './sku';
 const router = Router();
 
 router.get('/', (_req: Request, res: Response): any => res.json({message: "Welcome to my API! There's nothing here..."}));
-router.get('/sku', sku);
+router.use('/sku', sku);
 
 export default router;
--- src/routes/sku.ts
+++ src/routes/sku.ts
@@ -4,24 +4,29 @@ import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-router.get('/', (_req: Request, res: Response): any => res.json({ message: "Welcome to my API! There's nothing here..." }));
 router.get('/:skuId', async (req: Request, res: Response) => {
   const { skuId } = req.params;
-  
-  // 3350033630314
-  const barcodes: IBarcodes | null = await getBarcodes(skuId);
-  if (!barcodes)
-    return res.json({ error: "SKU introuvable." });
-  
-  const skus: ISkus | null = await getSkus(barcodes.skuId);
-  await addRow({
-    'MPX': barcodes.skuId,
-    'Code-barre': barcodes.barcode,
-    'Asset Fill': barcodes.assetFill,
-    'Designation': skus.description,
-    'Zones': skus.receivableZones,
-  });
-  res.json({ success: "Good." });
+  // sku id example: 3350033630314
+
+  try {
+    const barcodes: IBarcodes | null = await getBarcodes(skuId);
+    if (!barcodes)
+      return res.json({ error: "SKU not found." });
+
+    const skus: ISkus | null = await getSkus(barcodes.skuId);
+    await addRow({
+      'MPX': barcodes.skuId,
+      'Code-barre': barcodes.barcode,
+      'Asset Fill': barcodes.assetFill,
+      'Designation': skus.description,
+      'Zones': skus.receivableZones,
+    });
+
+    res.json({ success: "Good." });
+  } catch (e) {
+    console.error(e);
+    res.json({ error: "Internal Error." });
+  }
 });
 
 export default router;
